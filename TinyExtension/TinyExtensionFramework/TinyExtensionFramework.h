cd0a131d0dd91de92cb860625343935e29de90d2
Galvin <b02437@gmail.com>
2019-01-16 16:30:26 +0800

    add framework target for playground ready

---
+++ TinyExtension/TinyExtensionFramework/TinyExtensionFramework.h
@@ -0,0 +1,19 @@
+//
+//  TinyExtensionFramework.h
+//  TinyExtensionFramework
+//
+//  Created by Galvin on 2019/1/16.
+//  Copyright © 2019 @GalvinLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinyExtensionFramework.
+FOUNDATION_EXPORT double TinyExtensionFrameworkVersionNumber;
+
+//! Project version string for TinyExtensionFramework.
+FOUNDATION_EXPORT const unsigned char TinyExtensionFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyExtensionFramework/PublicHeader.h>
+
+
